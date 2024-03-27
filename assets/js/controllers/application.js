99466a3bf742dc4dfb2df57534813dc1ff2eb74e
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-03-27 14:05:36 +0000

    removed stimulus-flatpickr

--- assets/js/controllers/application.js
+++ assets/js/controllers/application.js
@@ -2,8 +2,8 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-import Flatpickr from 'stimulus-flatpickr'
-application.register('flatpickr', Flatpickr)
+//import Flatpickr from 'stimulus-flatpickr'
+//application.register('flatpickr', Flatpickr)
 
 // Configure Stimulus development experience
 application.debug = true
--- assets/js/controllers/index.js
+++ assets/js/controllers/index.js
@@ -1,5 +1,4 @@
 import { application } from 'controllers/application'
-console.log('testing before')
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 eagerLoadControllersFrom("controllers", application)
-console.log('testing after')
